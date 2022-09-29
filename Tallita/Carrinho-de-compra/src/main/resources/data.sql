21245706435b5396137cc36c843daa510de1f584
talli-ns <tallita.sn@gmail.com>
2022-09-28 23:15:04 -0300

    configuração do data.sql e população de tabelas

---
+++ Tallita/Carrinho-de-compra/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO cliente (id, cep, complemento, nome) VALUES
+(1l, '123456', 'Rua Cliente 1', 'Maria'),
+(2l, '123457', 'Rua Cliente 2', 'Joao'),
+(3l, '123458', 'Rua Cliente 3', 'Joaquim');
+
+INSERT INTO restaurante (id, cep, complemento, nome) VALUES
+(1l, '100001', 'Loja 01', 'Comida Japonesa'),
+(2l, '100002', 'Loja 02', 'Sorvetes e Açai'),
+(3l, '100003', 'Loja 03', 'Pasteis da Juju');
+
+INSERT INTO produto (id, disponivel, nome, valor_unitario, restaurante_id) VALUES
+(1l, 1, 'Carioca Sushi', 2.0, 1l),
+(2l, 1, 'Joy Joy Sushi', 3.0, 1l),
+(3l, 1, 'Cupuaçu na Tigela', 20.0, 2l),
+(4l, 1, 'Acai na Tigela', 25.0, 2l),
+(5l, 1, 'Pastel de Palmito', 2.0, 3l),
+(6l, 1, 'Pastel de Queijo', 2.0, 3l);
+
+INSERT INTO sacola (id, fechada, forma_pagamento, valor_total, cliente_id) VALUES
+(1l, 'false', '1',  100.0, 2l);
+
+--INSERT INTO item (id, sacola_id) VALUES
+--(1l, 1l);
--- Tallita/Carrinho-de-compra/src/main/resources/templates/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO cliente (id, cep, complemento,nome) VALUES
-(1, '123', 'abc', 'teste');
-(2, '123', 'def', 'teste2');
