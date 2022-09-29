b1d150d97761c39f6048001cc41bc3b1d91647a0
talli-ns <tallita.sn@gmail.com>
2022-09-29 19:11:54 -0300

    inclusão do Repository

---
+++ Tallita/Carrinho-de-compra/src/main/java/me/dio/sacola/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package me.dio.sacola.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.qos.logback.core.net.server.Client;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Client, Long> {
+
+}
